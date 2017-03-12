b0b6ee5a56ffd3d46cf794792a6376244b281790
kovalenkovpu <t085en@gmail.com>
2017-03-12 21:19:23 +0300

    user pug

--- components/user/user.js
+++ components/user/user.js
@@ -1,9 +1,20 @@
 (function() {
   "use strict";
   
+  //import
+  const user_tmp = window.user_tmp;
+  
   class User {
     constructor() {
       this.name = this._getName();
+      
+      this.chatEl = document.querySelector(".chat-header");
+    }
+    
+    _createAvatar() {
+      let avatar = window.user_tmp();
+      
+      this.chatEl.appendChild();
     }
     
     _getName() {
